b9d4f6ca2146e866b92e9c9b1b811098512d5ce9
yasin <benomaryasin1980@gmail.com>
2025-02-26 14:40:59 +0100

    cambios dependencias

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
 module.exports = {
-    resolve: {
+  ignoreWarnings: [/Failed to parse source map/],
+  devtool: false,  
+  resolve: {
       fallback: {
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        "util": require.resolve("util/"),
-        "zlib": require.resolve("browserify-zlib"),
-        "assert": require.resolve("assert/"),
-        "url": require.resolve("url/"),
-        "http": require.resolve("stream-http")
+          "http": require.resolve("stream-http"),
+          "https": require.resolve("https-browserify"),
+          "stream": require.resolve("stream-browserify"),
+          "util": require.resolve("util/"),
+          "zlib": require.resolve("browserify-zlib"),
+          "assert": require.resolve("assert/"),
+          "url": require.resolve("url/"),
+          "http": require.resolve("stream-http")
       }
-    }
-  };
-  
+  },
+  plugins: [
+      new NodePolyfillPlugin(), 
+  ],
+  module: {
+      rules: [
+          {
+              test: /\.(js|jsx)$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: "babel-loader",
+                  options: {
+                      presets: ["@babel/preset-env", "@babel/preset-react"]
+                  },
+              },
+          },
+      ],
+  },
+};
