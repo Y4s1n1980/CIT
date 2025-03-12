d7c6bf4de7572c97b506efd501e19251e9c603d6
yasin <benomaryasin1980@gmail.com>
2025-03-12 10:02:26 +0100

    Fix: Eliminada ruta Mezquita y ajustes en configuración

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,39 @@
+const TerserPlugin = require("terser-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = {
   ignoreWarnings: [/Failed to parse source map/],
-  devtool: false,  
+  devtool: false,
   resolve: {
-      fallback: {
-          "http": require.resolve("stream-http"),
-          "https": require.resolve("https-browserify"),
-          "stream": require.resolve("stream-browserify"),
-          "util": require.resolve("util/"),
-          "zlib": require.resolve("browserify-zlib"),
-          "assert": require.resolve("assert/"),
-          "url": require.resolve("url/"),
-          "http": require.resolve("stream-http")
-      }
+    fallback: {
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "zlib": require.resolve("browserify-zlib"),
+      "assert": require.resolve("assert/"),
+      "url": require.resolve("url/")
+    }
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
   },
   plugins: [
-      new NodePolyfillPlugin(), 
+    new NodePolyfillPlugin() 
   ],
   module: {
-      rules: [
-          {
-              test: /\.(js|jsx)$/,
-              exclude: /node_modules/,
-              use: {
-                  loader: "babel-loader",
-                  options: {
-                      presets: ["@babel/preset-env", "@babel/preset-react"]
-                  },
-              },
-          },
-      ],
-  },
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
 };
