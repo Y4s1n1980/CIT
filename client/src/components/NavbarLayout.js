0e3f5558231fd543a9684c459e3bfe3008edf838
yasin <benomaryasin1980@gmail.com>
2025-03-26 14:22:59 +0100

    mejora banner de terminos

--- client/src/components/NavbarLayout.js
+++ client/src/components/NavbarLayout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import CookieBanner from './CookieBanner';
 
 const NavbarLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <CookieBanner />
     </>
   );
 };
